6832a17e97319c7ce29817f32602ac4309767ac1
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-21 23:51:36 +0530

    Add files via upload

---
+++ Nested_Dictionary.py
@@ -0,0 +1,10 @@
+students = {
+    "name" : "Shradha" ,
+    "Score" : {
+        "Chemistry" : 98 ,
+        "Physic" : 97 ,
+        "Maths" :96 ,
+    }
+}
+
+print(students["Score"]["Chemistry"])
