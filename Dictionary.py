27475ab796df2744b8ed91d2778754eca2f3d725
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-21 23:08:25 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+info = {
+    "key" : "value" ,
+    "name" : "atharv nasare" ,
+    "Learning" : " Coding" ,
+    "Age" :  21,
+    "Subject" : ["C" , "Python" , "java"] ,
+    12.99 : 9783.3
+    }
+
+print(info)
