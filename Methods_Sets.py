e0b03f93b4a63ca1ed566c376741d65a5265a474
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-23 00:15:31 +0530

    Different types of Sets Method
    ADD
    REMOVE
    CLEAR
    POP
    UNION
    INTERSECTION

---
+++ Methods_Sets.py
@@ -0,0 +1,12 @@
+collection = set()
+
+collection.add(1)
+collection.add(2)
+collection.add(2)
+collection.add("Apnacollege")  # Add an element 
+collection.add((3,4,5))
+
+#Set.remove (Remove the element)
+#Set.clear(Empty the set)
+print((collection))
+
