eae10638562cebfc83797a69cf0e8017d0287ee0
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-23 23:28:57 +0530

    Add files via upload
    To Remove a random value using POP

---
+++ Methods_Sets_1.py
@@ -0,0 +1,6 @@
+collection = {"Hello" , "Apna college" , "World" , " Coding" , "Python"}
+
+print(collection.pop())
+print(collection.pop())
+print(collection.pop())
+print(collection.pop()) #remove a random value
