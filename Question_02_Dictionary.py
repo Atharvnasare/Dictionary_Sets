75e5f7db3ccd3619f118d2faabd1f61b750a2235
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-23 23:57:41 +0530

    Add files via upload

---
+++ Question_02_Dictionary.py
@@ -0,0 +1,12 @@
+marks = {}
+
+x = int(input("Enter a Phy :"))
+marks.update({"Phy" : x})
+
+x = int(input("Enter a Math :"))
+marks.update({"Math" : x})
+
+x = int(input("Enter a Bio :"))
+marks.update({"Bio" : x})
+
+print(marks)
