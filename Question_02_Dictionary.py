9c1906f4c5650de4f8093ca4d6590a6f57406888
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-23 23:59:46 +0530

    Update Question_02_Dictionary.py

--- Question_02_Dictionary.py
+++ Question_02_Dictionary.py
@@ -1,3 +1,5 @@
+Write a Program to enter a marks of 3 subject from the uses and store them in a dictionary starts with an empty dictionary and add one by one 
+
 marks = {}
 
 x = int(input("Enter a Phy :"))
