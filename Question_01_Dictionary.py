cbbb64a8715bac496ff467dc48306f20a52789c9
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-24 00:01:36 +0530

    Update Question_01_Dictionary.py

--- Question_01_Dictionary.py
+++ Question_01_Dictionary.py
@@ -1,3 +1,5 @@
+You have given a list of a subject . assume one classroom is required for 1 sibject . How many classroom are needed by all student
+
 dictionary = {
     "cat" : "a small animal" ,
     "table" : ["a piece of furniture" , "list of facts & figures"]
