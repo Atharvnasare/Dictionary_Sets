d1c34cfa12207c96d61021e32b27473c8a93b193
Atharv Nasare <122720807+Atharvnasare@users.noreply.github.com>
2024-12-24 00:11:28 +0530

    Add files via upload

---
+++ Question_03_Dictionary.py
@@ -0,0 +1,8 @@
+# figure out a way to store 9 to 9.0 as seperate values in the set. (You can take a help of build in type data types)
+
+values = {
+    ("float", 9.0), 
+("int", 9)
+}
+print(values)
+
